b4d1b4ec2e983176e916e5898928031168efd8cc
Victor <vickyboy1610@gmail.com>
2026-02-08 23:46:33 +0100

    added event and create-event pages

---
+++ app/create-event/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h2>Creation of event is currently not available</h2>
+    </div>
+  )
+}
+
+export default page
---
+++ app/events/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EventsPage = () => {
+  return (
+    <div>
+      <h2>No upcoming event,Please visit again.Thank You</h2>
+    </div>
+  )
+}
+
+export default EventsPage
